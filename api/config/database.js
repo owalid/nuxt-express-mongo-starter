faf0689a8971ff663b169ca8ff1b8400ef3bf428
owalid <othmane.el.ayadi@gmail.com>
2021-10-18 23:57:32 +0200

    update repo

--- api/config/database.js
+++ api/config/database.js
@@ -4,13 +4,11 @@ require('dotenv').config()
 export const connectDb = async () => {
   try {
     console.log('Starting server')
-    mongoose.set('useFindAndModify', false)
     await mongoose.connect(
       `mongodb://public:\
       ${process.env.NUXT_DB_PASSWD}\
       @${process.env.NUXT_DB_HOST}\
-      /${process.env.NUXT_DB_NAME}`,
-      { useNewUrlParser: true, useUnifiedTopology: true }
+      /${process.env.NUXT_DB_NAME}`
     )
     console.log('Connect to db')
   } catch (err) {
